5f005f6a6209f7d69565f25e8e684f7020b7d032
Yash Thipsay <yashthpsay@gmail.com>
2023-09-06 12:16:13 +0530

    hardhat configuration

--- chain/hardhat.config.js
+++ chain/hardhat.config.js
@@ -1,6 +1,24 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-truffle5");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity:{ 
+    version: "0.8.19",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+  },
+},
+
+},
+defaultNetwork: "hardhat",
+networks: {
+  hardhat: {},
+  polygon: {
+    url: API_URL,
+    accounts: [`0x${PRIVATE_KEY}`],
+  }
+},
 };
